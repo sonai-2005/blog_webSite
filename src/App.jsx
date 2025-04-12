3a074ddaf816c5bca4b86140606c7476a5eca835
sonai-2005 <nandisonai60@gmail.com>
2025-04-12 19:48:43 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>A blog website with appwrite</h1>
+    </>
+      
+      
+  )
+}
+
+export default App
